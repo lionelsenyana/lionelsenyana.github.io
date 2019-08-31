5caa2f684c6ea8ecf820ada601f8de009b33f324
lionelsenyana <lionelsenyana@gmail.com>
2019-08-30 19:18:30 -0600

    added swagger

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const requestBodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+
 const authorizationRouter = require('./server/routes/auth-routes');
 const userRouter = require('./server/routes/user-routes');
 const mentorRouter = require('./server/routes/mentor-routes');
 const sessionRouter = require('./server/routes/session-routes');
+
+const swaggerDocument = require('./swagger.json');
 
 
 const app = express();
@@ -32,6 +36,7 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/mentor', mentorRouter);
 app.use('/api/v1/sessions', sessionRouter);
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Free mentors app listening on port ${port}.`));
 
